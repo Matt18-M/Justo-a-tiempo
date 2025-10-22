e631a0322ef07cc2f91a51ff034d96aa0a483ca8
miguelalejandro1991 <mibitech22@gmail.com>
2025-10-21 22:51:22 -0500

    mostrar movimiento

--- trabajo_grupal/movimientos.js
+++ trabajo_grupal/movimientos.js
@@ -16,8 +16,20 @@ cargar=function(){
 filtrarMovimientos=function(numeroCuenta){
     let movimientosCuenta=[];
     //Se barre el arreglo de movimientos
+    for(let i=0;i<movimientos.length;i++){
+        if(movimientos[i].numeroCuenta==numeroCuenta){
+            movimientosCuenta.push(movimientos[i]);
+        }
+    }
     //En cada iteración, verifica si el numero de cuenta del movimiento es igual al que recibe como parametro
+    for(let i=0;i<movimientosCuenta.length;i++){
+        if(movimientosCuenta[i].tipo=="D"){
+            movimientosCuenta[i].monto *= -1;
+        }
+    }
+
     //En caso de serlo, agrega la cuenta al arreglo movimientosCuenta
+    mostrarMovimientos(movimientosCuenta);
     //Invoca a mostrarMovimientos, pasándole como parámetro movimientosCuenta
 }
 
@@ -26,7 +38,10 @@ filtrarMovimientos=function(numeroCuenta){
 */
 mostrarMovimientos=function(misMovimientos){
     //Muestra en pantalla una tabla con los movimientos que recibe en misMovimientos
+    let cmpTabla= document.getElementById("tablaMovimientos");
+
     //Columnas: NUMERO CUENTA, MONTO, TIPO
+    let contenidoTabla = "<table><tr>" + "<th> CUENTA </th>" + "<th> MONTO </th>" + "<th> OPERACION </th>" + "</tr>";
     //Si ya pinta correctamente la tabla, hacer el siguiente cambio:
     //Si el tipo es D(DEBITO), mostrar el monto en negativo (multiplicar por -1)
     //Si el tipo es C(CREDITO), mostrar el monto en positivo (tal como está guardado)
