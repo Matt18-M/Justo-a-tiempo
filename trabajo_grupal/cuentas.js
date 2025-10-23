fa24cd6f49dfedf4ff0877800de42e4e82396c91
2Erik <erik19yogo@gmail.com>
2025-10-22 22:14:45 -0500

    Parde cuentas

--- trabajo_grupal/cuentas.js
+++ trabajo_grupal/cuentas.js
@@ -7,6 +7,7 @@ cargar=function(){
     mostrarComponente("divCuentas");
     ocultarComponente("divMovimientos");
     ocultarComponente("divTransacciones");
+    mostrarCuentas();
     
 }
 
@@ -16,6 +17,20 @@ mostrarCuentas=function(){
         Columnas: NUMERO CUENTA, NOMBRE, SALDO
         En la columna NOMBRE concatenar el nombre y el apellido
     */
+
+    let cmpTabla=document.getElementById("lblTabla");
+    let tabla="<table><tr>"+
+    "<th>NUMERO CUENTA</th>"+
+    "<th>NOMBRE</th>"+
+    "<th>SALDO</th></tr>";
+
+    for(let i=0;i<cuentas.length;i++){
+        tabla+="<tr><td>"+cuentas[i].numeroCuenta+"</td>"
+        +"<td>"+cuentas[i].nombre+" "+cuentas[i].apellido+"</td>"
+        +"<td>"+cuentas[i].saldo+"</td></tr>";
+    }
+    tabla+="</table>";
+    cmpTabla.innerHTML=tabla;
 }
 
 /*
@@ -23,7 +38,12 @@ mostrarCuentas=function(){
     si existe retorna el objeto cuenta, caso contrario retorna null. 
 */
 buscarCuenta=function(numeroCuenta){
-
+    let cuenta;
+    for(let i=0;i<cuentas.length;i++){
+        cuenta=cuentas[i];
+        if(numeroCuenta==cuenta.numeroCuenta)return cuenta;
+        else return null;
+    }
 }
 
 /*
@@ -33,6 +53,16 @@ buscarCuenta=function(numeroCuenta){
 agregarCuenta=function(cuenta){
     //Si ya existe mostrar un alert CUENTA EXISTENTE
     //Si se agrega, mostrar un alert CUENTA AGREGADA
+    for(let i=0;i<cuentas.length;i++){
+        if(cuentas[i].numeroCuenta==cuenta.numeroCuenta){
+            alert("CUENTA EXISTENTE");
+            return ;
+        }
+    }
+
+    cuentas.push(cuenta);
+    alert("CUENTA AGREGADA");
+
 }
 
 agregar=function(){
@@ -40,4 +70,16 @@ agregar=function(){
     //Crea un objeto cuenta y agrega los atributos con los valores de las cajas respectivas
     //Invoca a agregarCuenta
     //Invoca a mostrarCuentas
+    let numeroCuenta=recuperarTexto("txtNumeroCuenta");
+    let cedula=recuperarTexto("txtCedula");
+    let nombre=recuperarTexto("txtNombre");
+    let apellido=recuperarTexto("txtApellido");
+    let cuenta={};
+    cuenta.numeroCuenta=numeroCuenta;
+    cuenta.cedula=cedula;
+    cuenta.nombre=nombre;
+    cuenta.apellido=apellido;
+
+    agregarCuenta(cuenta);
+    mostrarCuentas();
 }
